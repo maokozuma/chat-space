ab74da57d8e4c58fd63d6bd68165dc54d193322f
上妻真緒 <kyarameruribon82@gmail.com>
2017-07-30 21:44:08 +0900

    delete migration file

--- db/migrate/20170713090431_create_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :users do |t|
-      t.string :name, null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
